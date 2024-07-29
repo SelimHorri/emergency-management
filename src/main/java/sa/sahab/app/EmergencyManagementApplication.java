7f89ee0a4260ff2077002a74e4e2685cd912bcdd
Selim Horri <selim.horri@amaris.com>
2024-07-29 10:11:42 +0100

    init codebase

---
+++ src/main/java/sa/sahab/app/EmergencyManagementApplication.java
@@ -0,0 +1,16 @@
+package sa.sahab.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyManagementApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyManagementApplication.class, args);
+	}
+	
+}
+
+
+
---
+++ src/test/java/sa/sahab/app/EmergencyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package sa.sahab.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
